3c2ab3c5a0e595c81afc15438c2d02a20cdb6adc
Basher259 <baltasarschwerdt@gmail.com>
2025-06-15 19:08:38 -0300

    deleted useless Payment

--- app/api/mercadopago.ts
+++ app/api/mercadopago.ts
@@ -4,7 +4,6 @@ import prisma from '../lib/prisma';
 
 const client = new MercadoPagoConfig({ accessToken: process.env.MP_ACCESS_TOKEN || '' });
 const preference = new Preference(client);
-const payment = new Payment(client);
 
 export async function createPreference(items: Item[]) {
   try {
