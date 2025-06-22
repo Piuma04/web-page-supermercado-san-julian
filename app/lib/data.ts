2fa6ab3b3af2c7e0e689f56d41f5cd62aae8c230
Franco Piumatti <francopiumatti2004@gmail.com>
2025-06-22 17:31:24 -0300

    solve a thing

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -93,3 +93,54 @@ export async function deleteItemFromCart(cartItemId:number) {
     revalidatePath("/(routes)/cart");
 }
 
+export async function addToCart(email: string, productId: number, quantity: number) {
+    const user = await prisma.user.findUnique({
+        where: {
+            email
+        }
+    })
+
+    if (!user) {
+        throw new Error("User not found.");
+    }
+
+    const cart = await prisma.cart.findUnique({
+        where: {
+            userId: user?.id
+        },
+        include: {
+            items: {
+            }
+        }
+    })
+
+    if (cart?.id === undefined) {
+            throw new Error("Cart not found for user.");
+        }
+    
+    const existingItem = cart?.items.find(item => item.productId === productId);
+    if(existingItem)
+    {
+        // Update existing item quantity
+        await prisma.cartItem.update({
+            where: {
+                id: existingItem.id
+            },
+            data: {
+                quantity: existingItem.quantity + quantity
+            }
+        });
+    }
+    else {
+        // Add new item to cart
+        await prisma.cartItem.create({
+            data: {
+                cartId: cart.id,
+                productId: productId,
+                quantity: quantity
+            }
+        });
+    }
+    return cart;
+}
+
