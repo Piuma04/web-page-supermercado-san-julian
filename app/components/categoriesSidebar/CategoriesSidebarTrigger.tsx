8befbfe5a453c51acd1e22cb5d3fdfed7ac3cdfe
Basher <baltasarschwerdt@gmail.com>
2025-06-24 17:13:10 -0300

    minor change to sidebar trigger

--- app/components/categoriesSidebar/CategoriesSidebarTrigger.tsx
+++ app/components/categoriesSidebar/CategoriesSidebarTrigger.tsx
@@ -16,11 +16,12 @@ export default function CategoriesSidebarTrigger({
       data-sidebar="trigger"
       data-slot="sidebar-trigger"
       variant="ghost"
-      className={className}
+      className={`hover:bg-transparent ${className}`}
       onClick={(event) => {
         onClick?.(event)
         toggleSidebar()
       }}
+      
       {...props}
     >
       <Menu className="size-4 lg:size-8" aria-label="Menú" size={32} />
