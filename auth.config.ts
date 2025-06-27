0476074af16a94df5042ade987befe6b36db0c79
Franco Piumatti <francopiumatti2004@gmail.com>
2025-06-27 13:50:15 -0300

    now cloudinary can be used

--- auth.config.ts
+++ auth.config.ts
@@ -10,8 +10,8 @@ export const authConfig = {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
       const isOnUserProfile = nextUrl.pathname.startsWith('/profile');
-      const isOnCart =     nextUrl.pathname.startsWith('/cart');
-      const isOnAdmin =     nextUrl.pathname.startsWith('/cart');
+      const isOnCart =  nextUrl.pathname.startsWith('/cart');
+      const isOnAdmin = nextUrl.pathname.startsWith('/cart');
       if (isOnUserProfile || isOnCart || isOnAdmin) {
         if (isLoggedIn) return true;
         return false; // Redirect unauthenticated users to login page
