b3217dd02c508056df6f0dff3d5f71afc3a4b20f
Basher259 <baltasarschwerdt@gmail.com>
2025-06-29 22:02:54 -0300

    small function name change

--- app/components/admin/AdminProductTable.tsx
+++ app/components/admin/AdminProductTable.tsx
@@ -1,6 +1,6 @@
 
 
-import { getFilteredProducts } from "@/app/lib/data";
+import { fetchFilteredProducts } from "@/app/lib/data";
 import AdminProductCard from "./AdminProductCard";
 
 
@@ -11,7 +11,7 @@ type Props = {
 };
 
 export default async function ProductGrid({ query, currentPage }: {query:string, currentPage:number}) {
-  const products = await getFilteredProducts(query, currentPage);
+  const products = await fetchFilteredProducts(query, currentPage);
 
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
