4a5c874452bf12cf8b45d83784eee9de474700a6
Franco Piumatti <francopiumatti2004@gmail.com>
2025-07-05 00:17:07 -0300

    oaaaaaaa

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,32 @@
 
-import NextAuth from 'next-auth';
 import { getToken } from 'next-auth/jwt';
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
-import { authConfig } from './auth.config';
 
-export default NextAuth(authConfig).auth;
+export default async function middleware(req: NextRequest) {
+  const token = await getToken({ req , secret: process.env.AUTH_SECRET});
+  const url = req.nextUrl.clone();
+  const pathname = req.nextUrl.pathname;
 
+  const privateRoutes = ['/admin', '/cart', '/profile',];
+  if (!privateRoutes.includes(pathname)) {
+    return NextResponse.next();
+  }
+
+
+  if (!token) {
+    url.pathname = '/login';
+    return NextResponse.redirect(url);
+  }
+
+
+  if (pathname.startsWith('/admin') && token.role !== 'ADMIN') {
+    url.pathname = '/';
+    return NextResponse.redirect(url);
+  }
+
+  return NextResponse.next();
+}
 export const config = {
   
   matcher: [
