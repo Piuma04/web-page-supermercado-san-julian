970567b723bf912ad9d724446528ea6e56035ba6
Basher259 <baltasarschwerdt@gmail.com>
2025-07-07 11:16:41 -0300

    uncommented pwa thing

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <meta name="theme-color" content="#ff0000" />
                 <link rel="icon" href="/images/favicon.ico" />
-                {/*<link rel="manifest" href="/manifest.json" />*/}
+                <link rel="manifest" href="/manifest.json" />
                 <link rel="apple-touch-icon" href="/images/icon.png" />
                 <meta name="apple-mobile-web-app-capable" content="yes" />
                 <meta name="apple-mobile-web-app-status-bar-style" content="default" />
