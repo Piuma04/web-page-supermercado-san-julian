cb55b18adca08f418200a28c3dcdf2b9ce9c1d47
Basher259 <baltasarschwerdt@gmail.com>
2025-07-09 16:04:36 -0300

    removed unused login suspense

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,14 +1,10 @@
-import { Suspense } from 'react';
 import LoginForm from '@/app/components/LoginForm';
-import { signIn } from '@/auth';
 
 export default function LoginPage() {
   return (
     <main>
       <div>
-        <Suspense>
           <LoginForm />
-        </Suspense>
       </div>
     </main>
   );
